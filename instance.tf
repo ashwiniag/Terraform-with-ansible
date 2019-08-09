a180159e74da56e6fe654a22d02393b9661e6b7d
ashwiniag <ashwinigaddagiwork@gmail.com>
2019-08-09 07:48:44 -0400

    create instance

---
+++ instance.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "test-ec2-instance" {
+  ami = "${var.ami_id}"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.ingress-all-test.id}"]
+  tags = {
+    Name = "${var.ami_name}"
+  }
+subnet_id = "${aws_subnet.subnet-uno.id}"
+}
